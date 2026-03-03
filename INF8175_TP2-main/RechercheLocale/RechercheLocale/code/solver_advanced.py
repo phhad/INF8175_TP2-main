e2fd8ea9573e580aaf820792d070c7d9a7bc358b
phhad <philippe.hadjis@polymtl.ca>
2026-03-02 22:38:02 -0500

    Fonction solution initiale et début voisinage

--- INF8175_TP2-main/RechercheLocale/RechercheLocale/code/solver_advanced.py
+++ INF8175_TP2-main/RechercheLocale/RechercheLocale/code/solver_advanced.py
@@ -1,3 +1,5 @@
+import random as r
+
 def solve(schedule):
     """
     Your solution of the problem
@@ -5,9 +7,17 @@ def solve(schedule):
     :return: a list of tuples of the form (c,t) where c is a course and t a time slot. 
     """
     # Add here your agent
-    solution = ()
+    solution = generate_random_solution(schedule)
     
+    def generate_random_solution(schedule):
+        solution = dict()
+        for c in schedule.course_list:
+            choix = r.randint(1, len(schedule.course_list))
+            solution[c] = choix
+        schedule.get_n_creneaux(solution)
+        return solution
+    
+    def two_swap_neighborhood(solution):
+        neighbourhood = []
     
     return solution
-
-    raise Exception("Agent is not implemented")
