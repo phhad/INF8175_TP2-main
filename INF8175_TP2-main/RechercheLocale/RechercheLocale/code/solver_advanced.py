fb13f31e7e15097a54bc2cdaa0f4585d909db0d1
phhad <philippe.hadjis@polymtl.ca>
2026-03-03 16:18:39 -0500

    Premiere version du solver_advanced

--- INF8175_TP2-main/RechercheLocale/RechercheLocale/code/solver_advanced.py
+++ INF8175_TP2-main/RechercheLocale/RechercheLocale/code/solver_advanced.py
@@ -8,16 +8,37 @@ def solve(schedule):
     """
     # Add here your agent
     solution = generate_random_solution(schedule)
+    neighbourhood = two_swap_neighbourhood(solution)
+    valid_neighbourhood = is_improving_solution(schedule, neighbourhood, solution, evaluation_function=evaluation_number_conficts)
     
-    def generate_random_solution(schedule):
-        solution = dict()
-        for c in schedule.course_list:
-            choix = r.randint(1, len(schedule.course_list))
-            solution[c] = choix
-        schedule.get_n_creneaux(solution)
-        return solution
-    
-    def two_swap_neighborhood(solution):
-        neighbourhood = []
+    while valid_neighbourhood:
+        solution = min(valid_neighbourhood, key=lambda n: evaluation_number_conficts(schedule, n))
+        neighbourhood = two_swap_neighbourhood(solution)
+        valid_neighbourhood = is_improving_solution(schedule, neighbourhood, solution, evaluation_function=evaluation_number_conficts)
+        n += 1
     
     return solution
+    
+def generate_random_solution(schedule):
+    solution = dict()
+    for c in schedule.course_list:
+        choix = r.randint(1, len(schedule.course_list))
+        solution[c] = choix
+    return solution
+    
+def two_swap_neighbourhood(solution):
+    neighbourhood = []
+    for c1 in solution:
+        for c2 in solution:
+            if c1 != c2:
+                n = solution.copy()
+                n[c1], n[c2] = n[c2], n[c1]
+                neighbourhood.append(n)
+    return neighbourhood
+    
+def is_improving_solution(schedule, neighbourhood, solution, evaluation_function):
+    return [n for n in neighbourhood if evaluation_function(schedule, n) < evaluation_function(schedule, solution)]
+        
+def evaluation_number_conficts(schedule, solution):
+    return sum(solution[a[0]] == solution[a[1]] for a in schedule.conflict_list)    
+    
